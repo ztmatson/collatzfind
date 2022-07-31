33b5951f5c95b09a00c21c29a23cbc8e71428529
ztmatson <110339584+ztmatson@users.noreply.github.com>
2022-07-31 14:00:30 -0400

    collatz a number (broken)

---
+++ collatzone.py
@@ -0,0 +1,25 @@
+from unittest import result
+
+
+def collatz(number):
+    steps = 0
+
+    if number % 2 == 0:
+        print(number // 2)
+        number /= 2
+        steps += 1
+        return
+
+    elif number % 2 == 1:
+        result = 3 * number + 1
+        steps += 1
+        return result
+
+steps = 0
+
+i = int(input("Enter a number: "))
+while i != 1:
+    i = collatz(int(i))
+while i == 1:
+    print('Ran into 1 in ' + str(steps) + ' steps')
+    break 
